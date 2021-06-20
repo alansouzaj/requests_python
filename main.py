86f9aeb0f603fcbd0108e28a02fb249543e65b59
alansouzaj <jose.alansouza@gmail.com>
2021-06-20 18:14:07 -0300

    changed save_path

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 import os
 
-save_path = '/Users/jose.souza/repos_personal/requests_python/download'
+save_path = f'{os.getcwd()}/download'
 download_url = 'https://binaries.templates.cdn.office.net/support/templates/en-us/tf16402488_win32.dotx'
 
 req = requests.get(download_url)
 
 filename = req.url[download_url.rfind('/')+1:]
-complete_path = os.path.join(save_path, filename)
+file_path = os.path.join(save_path, filename)
 
-open(f'{complete_path}', 'wb').write(req.content)
+open(f'{file_path}', 'wb').write(req.content)
 
-print(complete_path)
+print(file_path)
